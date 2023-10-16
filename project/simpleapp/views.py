c7fcfc900523e0549e880081e26456a3786f7c44
Semyon <138588831+MrSemy@users.noreply.github.com>
2023-10-17 00:24:32 +0800

    Add files via upload

---
+++ project/simpleapp/views.py
@@ -0,0 +1,86 @@
+from django.shortcuts import render
+from django.views.generic import (ListView, DetailView, CreateView, UpdateView, DeleteView)
+
+from .models import Post
+from .filters import PostFilter
+from django.urls import reverse_lazy
+from .forms import PostForm
+
+
+class PostsList(ListView):
+    model = Post
+    ordering = '-date_of_post'
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+    paginate_by = 10
+
+
+    def get_queryset(self):
+       # Получаем обычный запрос
+       queryset = super().get_queryset()
+       # Используем наш класс фильтрации.
+       # self.request.GET содержит объект QueryDict, который мы рассматривали
+       # в этом юните ранее.
+       # Сохраняем нашу фильтрацию в объекте класса,
+       # чтобы потом добавить в контекст и использовать в шаблоне.
+       self.filterset = PostFilter(self.request.GET, queryset)
+       # Возвращаем из функции отфильтрованный список товаров
+       return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+       context = super().get_context_data(**kwargs)
+       # Добавляем в контекст объект фильтрации.
+       context['filterset'] = self.filterset
+       return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+
+class PostCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.news_or_article = 'news'
+        return super().form_valid(form)
+
+
+class PostUpdate(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('posts')
+
+
+class ArticleCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.news_or_article = 'article'
+        return super().form_valid(form)
+
+
+class ArticleUpdate(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+
+class ArticleDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('posts')
